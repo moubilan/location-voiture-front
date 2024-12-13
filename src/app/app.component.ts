819bf54596544e1d76350b9ffe1da0cfa606fbd2
Abdellatif Oubenmoh <mr.oubenmoh@gmail.com>
2024-12-13 14:16:44 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaConfig, FaIconComponent, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaIconComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'location-voiture-front';
+export class AppComponent implements OnInit{
+  
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+  
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+  
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'fas';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
